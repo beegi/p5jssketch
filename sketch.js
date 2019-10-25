fa64c927fb8c4a47c7485b36e12ef3bae3e9d350
beegi <beegi@users.noreply.github.com>
2019-10-25 14:59:08 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,39 +1,81 @@
-let pp = 0; //passpartout
-let lanz = 0; // lamellen anzahl
-let lab = 0; // lamellen abstand
-let linien = [];
+var winkel = 0;
+var radius = 60;
 
 function setup() {
-  createCanvas(600, 600);
-
-  pp = width / 5;
-  lab = 10;
-  lanz = round(((width - pp) / lab) * 1.06);
-  for (let i = 0; i < lanz; i++) {
-    linien[i] = new Linie(pp, lab * i, width - pp, lab * i); //bei 500 - 8, bei 600 -8.2
-  }
- }
+  createCanvas(windowWidth, windowHeight);
+}
 
 function draw() {
   background(250);
-  for (let i = 0; i < linien.length; i++) {
-    linien[i].move();
-    linien[i].show();
+  stroke(112, 70, 140);
+  strokeWeight(2);
+
+
+  var h = radius * sin(PI / 3);
+  var d = radius + radius * sin(PI / 6);
+  var l = width / radius;
+  for (var j = 0; j < l; j++) {
+    for (var i = 0; i < l; i++) {
+      push();
+      translate(h + 4 * i * h, d * (2 * j + 1));
+      rotate(winkel);
+      speichen(0, 0);
+      pop();
+    }
   }
 
-  noStroke();
-  noFill();
-  rect(pp, pp, width - pp, height - pp);
-
-  for (let i = 0; i < linien.length; i++) {
-    linien[i].move();
-    linien[i].show();
+  for (var n = 0; n < l; n++) {
+    for (var k = 1; k < l; k++) {
+      push();
+      translate(h + (4 * k - 2) * h, d * (2 * n + 1));
+      rotate(-winkel);
+      speichen(0, 0);
+      pop();
+    }
   }
 
-  fill(250);
-  noStroke();
-  rect(0, 0, width, pp);
-  rect(0, width - pp, width, width);
-  rect(0, pp, pp, height - 2 * pp);
-  rect(width - pp, pp, pp, height - 2 * pp);
+
+  for (var q = 0; q < l; q++) {
+    for (var p = 0; p < l; p++) {
+      push();
+      translate(4 * p * h, 2 * q * d);
+      rotate(winkel);
+      speichen(0, 0);
+      pop();
+    }
+  }
+
+  for (var m = 0; m < l; m++) {
+    for (var o = 1; o < l; o++) {
+      push();
+      translate((4 * o - 2) * h, 2 * m * d);
+      rotate(-winkel);
+      speichen(0, 0);
+      pop();
+    }
+  }
+
+  if (winkel < PI / 6) {
+    var easing = 0.05;
+    var diffX = PI / 6 - winkel;
+    winkel += diffX * easing;
+    winkel = winkel + 0.00005;
+  } else if (winkel > PI / 6 && winkel < PI / 3) {
+    easing = 0.05;
+    diffX = PI / 3 - winkel;
+    winkel += diffX * easing;
+    winkel = winkel + 0.00005;
+
+  } else winkel = 0;
+}
+
+function speichen(x, y) {
+  var r = radius;
+  line(x - r, y, x + r, y);
+  var x1 = x - r * cos(PI / 3);
+  var y1 = y + r * sin(PI / 3);
+  var x2 = x + r * cos(PI / 3);
+  var y2 = y - r * sin(PI / 3);
+  line(x1, y1, x2, y2);
+  line(x1, y2, x2, y1);
 }
